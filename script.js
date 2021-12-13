8620ad87567b258968d07d63f70f23a82add1e32
Hari sudhan <harisudhan1008@gmail.com>
2021-12-13 08:58:40 +0530

    first commit

--- script.js
+++ script.js
@@ -29,25 +29,32 @@ function find({data}) {
 
 function fetchphones(phones){
     let result= document.getElementById('result');
-    result.innerHTML=` <div id="sub"></div>`
-  phones.map(async(phone)=>{
+    result.innerHTML=`<hr id="hrr"> <div id="sub"></div>`
+     phones.map(async(phone)=>{
     let fetch_url = await fetch(phone.detail);
     let convert = await fetch_url.json();
     let data = convert.data;
-    show(data);
+     show(data);
   });
 }
 
-function show({phone_name,thumbnail,specifications}){
+function show({phone_name,thumbnail,specifications,os}){
     
     let sub= document.getElementById('sub');
-    let div1=document.createElement('div');
+    let div1= document.createElement('div');
     div1.id="card";
-    div1.innerHTML=
+    sub.appendChild(div1);
+    div1.innerHTML= 
     `<img src="${thumbnail}" alt="${phone_name}">
     <h5>${phone_name}</h5>
-    <p>${specifications[5].specs[1].val}</p>
-    <p>${specifications[4].specs[1].val}</p>`
-    
-    sub.appendChild(div1);
+    <p>Internal: ${specifications[5].specs[1].val}</p>
+    <p>Chip: ${specifications[4].specs[1].val}</p>
+    <p>OS: ${os}</p>`
 }
+
+document.getElementById('inputfield').addEventListener("keyup",function(event){
+  
+    if(event.key=="Enter"){
+      searchh();
+    }
+  });
